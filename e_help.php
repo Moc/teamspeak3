0a25e6afb7d8cd2b03b840c0dcc1f0c130f8649a
Moc <tijnkuyper@gmail.com>
2014-02-05 15:44:47 +0100

    Added help file to display minimal documentation in the admin area

---
+++ e_help.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * TeamSpeak 3 plugin
+ *
+ * Copyright (C) 2014 - Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+$action = vartrue($_GET['action']);
+
+switch(vartrue($action)) {
+   case 'list':
+   {
+      $text = LAN_TS3_H_02;
+      break;
+   }
+   case 'create':
+   {
+      $text = LAN_TS3_H_03;
+      break;
+   }
+   case 'prefs':
+   {
+      $text = LAN_TS3_H_04;
+      break;
+   }
+   default: 
+   {
+      $text = "";
+   }
+}
+
+if($text)
+{
+	$ns -> tablerender(LAN_TS3_H_01, $text);	
+}
+
+?>
