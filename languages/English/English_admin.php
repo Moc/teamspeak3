061a6d6679e19830fba8a5d404b9db7a74079006
Gábor <yesszus@gmail.com>
2016-12-20 15:16:36 +0100

    Update English_admin.php

--- languages/English/English_admin.php
+++ languages/English/English_admin.php
@@ -7,23 +7,24 @@ define("LAN_TS3_QPORT", "Query Port");
 define("LAN_TS3_ZONE", 	"Zone");
 
 // Preferences
-define("LAN_TS3_DEVMODE",		"TS3 Developer mode");
-define("LAN_TS3_FLAGS",			"Show country flag");
-define("LAN_TS3_CACHING",		"Caching");
-define("LAN_TS3_ADD_DATA", 		"Show additional data");
-define("LAN_TS3_CONN_BUTTON", 	"Show 'connect' button");
+define("LAN_TS3_DEVMODE", "TS3 Developer mode");
+define("LAN_TS3_FLAGS",   "Show country flag");
+define("LAN_TS3_CACHING", "Caching");
+define("LAN_TS3_ADD_DATA", "Show additional data");
+define("LAN_TS3_CONN_BUTTON", "Show 'connect' button");
 
 // Help sections
 // leave the [..] tags (e.g. [br]) unmodified! Do translate the words between {..} (e.g. {here})
 define("LAN_TS3_H_01",	"Information");
-define("LAN_TS3_H_02",
-						"
-						This page displays all the current TeamSpeak servers that you have entered. From here you can also modify each entry if required.
-						[br][br]
-						The full documentation of this plugin can be found {here}.
-						"
-);
+define("LAN_TS3_H_02", "This page displays all the current TeamSpeak servers that you have entered. From here you can also modify each entry if required.[br][br]The full documentation of this plugin can be found {here}.");
 define("LAN_TS3_H_03",	"Click {here} to get more information on adding new servers.");
 define("LAN_TS3_H_04",	"Click {here} to get more information on the preferences.");
-
+define("LAN_TS3_H_05",	"Developer mode is used for testing and development. Do not enable unless specifically advised!");
+define("LAN_TS3_H_06",	"Caching saves a recent copy of the data to avoid reaching the flood limit (see docs).");
+define("LAN_TS3_H_07",	"Shows the country flag of each client in the viewer");
+define("LAN_TS3_H_08",	"Shows a connect button which allows people to connect to your server through the viewer");
+define("LAN_TS3_H_09",	"Shows additional information below each teamspeak viewer such as the number of active clients");
+define("LAN_TS3_H_10",	"Port of the server. Default is 9987.");
+define("LAN_TS3_H_11",	"Query port. Default is 10011.");
+define("LAN_TS3_H_12",	"In which menu zone should this server be shown?");
 ?>
