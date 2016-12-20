f45889ff8cc64925f5e4e36d7a6655348b4947bd
Gábor <yesszus@gmail.com>
2016-12-20 15:00:54 +0100

    Create Hungarian_front.php

---
+++ languages/Hungarian/Hungarian_front.php
@@ -0,0 +1,18 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/12/20 11:18:45
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("PAGE_NAME", "TeamSpeak 3");
+define("LAN_TS3_001", "Aktív felhasználók");
+define("LAN_TS3_E_01", "Hiba történt a szerver lekérdezésekor! Kérjük, lépjen kapcsolatba az Adminisztrátorral.");
+define("LAN_TS3_E_02", "Kapcsolódás");
+
+?>
