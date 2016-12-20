1f0d7c0ac201c2a2de37fca15637e220b78264eb
Gábor <yesszus@gmail.com>
2016-12-20 15:01:29 +0100

    Update English_front.php

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -9,6 +9,6 @@ define("LAN_TS3_001", "Active clients");
 
 // Errors
 define("LAN_TS3_E_01", "Error when quering the server. Please contact the site administrator.");
-
+define("LAN_TS3_E_02", "Connect");
 
 ?>
