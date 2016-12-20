489be0bf52f778d4173072e0d9dad4152a99ddec
Gábor <yesszus@gmail.com>
2016-12-20 15:44:27 +0100

    Add files via upload

---
+++ languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,32 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/12/20 14:04:14
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+define("LAN_TS3_PORT", 	"Port");
+define("LAN_TS3_QPORT", "Lekérdezési (Query) Port");
+define("LAN_TS3_ZONE", 	"Zóna");
+define("LAN_TS3_DEVMODE", "TS3 Fejlesztői mód");
+define("LAN_TS3_FLAGS", "Ország zászlók megjelenítése");
+define("LAN_TS3_CACHING", "Cachelés");
+define("LAN_TS3_ADD_DATA", "Hozzáadott adatok megjelenítése");
+define("LAN_TS3_CONN_BUTTON", "'Kapcsolódás' gomb megjelenítése");
+define("LAN_TS3_H_01",	"Információ");
+define("LAN_TS3_H_02",	"Ez az oldal jeleníti meg az Ön által megadott TeamSpeak szervereket.[br]Itt módosíthatja az összes bejegyzést, ha szükséges.[br][br]{Itt} megtalálható a teljes plugin dokumentáció.");
+define("LAN_TS3_H_03",	"Klikk {ide} a további új szerverek hozzáadása információkhoz.");
+define("LAN_TS3_H_04",	"Klikk {ide} a további beállítás információkhoz.");
+define("LAN_TS3_H_05",	"Fejlesztői mód a tesztelésre és fejlesztésre. Ne engedélyezze ezt, ha nem feltétlenűl szükséges!");
+define("LAN_TS3_H_06",	"Cache mentés a közelmúlt adatok másolatáról, hogy ne ismétlődjenek az adatok (lásd dokumentum).");
+define("LAN_TS3_H_07",	"Megjeleníti a kapcsolódott felhasználó országának zászlaját.");
+define("LAN_TS3_H_08",	"Megjeleníti a kapcsolódás gombot, amely lehetővé teszi a felhasználóknak, hogy csatlakozzanak a szerverre.");
+define("LAN_TS3_H_09",	"Megjeleníti a további információkat és az aktív felhasználók számát.");
+define("LAN_TS3_H_10",	"Szerver Port. Alapértelmezett: 9987.");
+define("LAN_TS3_H_11",	"Lekérdezési Port. Alapértelmezett: 10011.");
+define("LAN_TS3_H_12",	"Melyik menü zónában jelenjen meg ez a szerver?");
+?>
