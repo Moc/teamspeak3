3b9a097811c3d44fdec43441e7e96636cf2962df
Gábor <yesszus@gmail.com>
2016-12-20 16:06:57 +0100

    Update Hungarian_front.php

--- languages/Hungarian/Hungarian_front.php
+++ languages/Hungarian/Hungarian_front.php
@@ -14,5 +14,7 @@ define("PAGE_NAME", "TeamSpeak 3");
 define("LAN_TS3_001", "Aktív felhasználók");
 define("LAN_TS3_E_01", "Hiba történt a szerver lekérdezésekor! Kérjük, lépjen kapcsolatba az Adminisztrátorral.");
 define("LAN_TS3_E_02", "Kapcsolódás");
-
+define("LAN_TS3_E_03", "Szükséges PHP minimum verzió: 5.3");
+define("LAN_TS3_E_04", "Ez az oldal részletes információkat jelenít meg egy adott TeamSpeak 3 szerverről.<br />Munkálatok folyamatban!");
+define("LAN_TS3_E_05", "TeamSpeak 3 - Részletes információk");
 ?>
