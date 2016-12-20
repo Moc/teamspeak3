d284f8f99dafe693096c62825bd3e1ce6d9fdc29
Gábor <yesszus@gmail.com>
2016-12-20 16:03:13 +0100

    Update English_front.php

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -10,5 +10,7 @@ define("LAN_TS3_001", "Active clients");
 // Errors
 define("LAN_TS3_E_01", "Error when quering the server. Please contact the site administrator.");
 define("LAN_TS3_E_02", "Connect");
-
+define("LAN_TS3_E_03", "A minimum version of PHP 5.3 is required.");
+define("LAN_TS3_E_04", "This page will display detailed information about a specific teamspeak 3 server.<br />Work in progress!");
+define("LAN_TS3_E_05", "TeamSpeak 3 - Detailed information");
 ?>
